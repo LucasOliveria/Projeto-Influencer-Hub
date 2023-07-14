206009fac9203943096c3ad0f2ba69fcd7469d32
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-07-14 02:01:06 -0300

    Página de deletar influencer concluída

--- src/app/components/container-delete/container-delete.component.ts
+++ src/app/components/container-delete/container-delete.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Influencer } from 'src/interfaces/interfaces';
+import api from 'src/service/api';
+import { getItem } from 'src/utils/storage';
 
 @Component({
   selector: 'app-container-delete',
   templateUrl: './container-delete.component.html',
   styleUrls: ['./container-delete.component.css']
 })
-export class ContainerDeleteComponent {
+export class ContainerDeleteComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
+  token: string | null = "";
+
+  nameInfluencer?: Influencer;
+
+  ngOnInit(): void {
+    this.token = getItem("token");
+
+    if (!this.token) {
+      this.router.navigate(["/"]);
+      return;
+    }
+
+    this.getInfluencer();
+  }
+
+  async getInfluencer(): Promise<void> {
+    const idInfluencer = this.route.snapshot.paramMap.get("id");
+
+    try {
+      const response = await api.get(`/influencers/${idInfluencer}`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`
+        }
+      });
+
+      this.nameInfluencer = response.data;
+    } catch (error: any) {
+      console.log(error.response.data);
+
+    }
+  }
+
+  async deleteInfluencer(): Promise<void> {
+    const idInfluencer = this.route.snapshot.paramMap.get("id");
+
+    try {
+      const response = await api.delete(`/influencers/${idInfluencer}`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`
+        }
+      });
+
+      console.log(response.data);
+
+      this.handleGoToHome();
+    } catch (error: any) {
+      console.log(error.response.data);
+
+    }
+  }
+
+  handleGoToHome() {
+    this.router.navigate(["/home"]);
+  }
 }
