f59a37ac56ea2c633362da39b3f3347c60568b87
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2024-05-21 09:24:34 -0300

    Trocar API Cyclic pela API Render

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://api-projeto-influencer-hub.cyclic.app',
+  baseURL: 'https://api-projeto-influencer-hub-alternative.onrender.com/',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 });
